424e06dee048a6f94ae4f255ccd57a9a73fb3856
moonrailgun <moonrailgun@gmail.com>
2020-09-28 10:40:57 +0800

    add collapsible sidebar
    refactor layout position of code and add Home Icon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Link from 'next/link';
 import Head from 'next/head';
 import { Layout, Menu } from 'antd';
@@ -6,9 +6,11 @@ import {
   AppstoreOutlined,
   CodeSandboxOutlined,
   GithubOutlined,
+  HomeOutlined,
 } from '@ant-design/icons';
 import { routes, RouteItem } from '../routes';
 import styled from 'styled-components';
+import { useLocalStorage } from 'react-use';
 
 const ContentMain = styled.div`
   flex: 1;
@@ -31,6 +33,69 @@ const BaseLayout: React.FC<{
   link?: string;
 }> = React.memo((props) => {
   const { title, link = '/', children } = props;
+  const [collapsed, setCollapsed] = useLocalStorage('collapsed', false);
+
+  const toggleCollapsed = useCallback(() => {
+    setCollapsed(!collapsed);
+  }, [collapsed]);
+
+  const renderSidebar = () => {
+    return (
+      <Layout.Sider
+        theme="light"
+        width={200}
+        collapsible={true}
+        collapsed={collapsed}
+        onCollapse={toggleCollapsed}
+      >
+        <Menu
+          mode="inline"
+          selectedKeys={[link]}
+          defaultOpenKeys={Object.keys(routes)}
+          style={{ height: '100vh' }}
+        >
+          <Menu.Item key="/" icon={<HomeOutlined />}>
+            <Link href="/">
+              <a>主页</a>
+            </Link>
+          </Menu.Item>
+          <Menu.SubMenu key="tools" icon={<AppstoreOutlined />} title="工具箱">
+            {routes['tools'].map(renderLink)}
+          </Menu.SubMenu>
+          <Menu.SubMenu
+            key="sandbox"
+            icon={<CodeSandboxOutlined />}
+            title="沙盒"
+          >
+            {routes['sandbox'].map(renderLink)}
+          </Menu.SubMenu>
+          <Menu.SubMenu key="github" icon={<GithubOutlined />} title="Github">
+            {routes['github'].map(renderLink)}
+          </Menu.SubMenu>
+        </Menu>
+      </Layout.Sider>
+    );
+  };
+
+  const renderContent = () => {
+    return (
+      <Layout.Content
+        style={{
+          padding: '12px 24px 0',
+          minHeight: 280,
+          display: 'flex',
+          flexDirection: 'column',
+          maxHeight: '100vh',
+          overflow: 'auto',
+        }}
+      >
+        <ContentMain>{children}</ContentMain>
+        <Layout.Footer style={{ textAlign: 'center' }}>
+          Powered by <a href="http://moonrailgun.com">moonrailgun</a> with ❤
+        </Layout.Footer>
+      </Layout.Content>
+    );
+  };
 
   return (
     <div style={{ height: '100vh' }}>
@@ -41,48 +106,8 @@ const BaseLayout: React.FC<{
       </Head>
 
       <Layout>
-        <Layout.Sider width={200}>
-          <Menu
-            mode="inline"
-            selectedKeys={[link]}
-            defaultOpenKeys={Object.keys(routes)}
-            style={{ height: '100vh' }}
-          >
-            {renderLink(['/', '主页'])}
-            <Menu.SubMenu
-              key="tools"
-              icon={<AppstoreOutlined />}
-              title="工具箱"
-            >
-              {routes['tools'].map(renderLink)}
-            </Menu.SubMenu>
-            <Menu.SubMenu
-              key="sandbox"
-              icon={<CodeSandboxOutlined />}
-              title="沙盒"
-            >
-              {routes['sandbox'].map(renderLink)}
-            </Menu.SubMenu>
-            <Menu.SubMenu key="github" icon={<GithubOutlined />} title="Github">
-              {routes['github'].map(renderLink)}
-            </Menu.SubMenu>
-          </Menu>
-        </Layout.Sider>
-        <Layout.Content
-          style={{
-            padding: '12px 24px 0',
-            minHeight: 280,
-            display: 'flex',
-            flexDirection: 'column',
-            maxHeight: '100vh',
-            overflow: 'auto',
-          }}
-        >
-          <ContentMain>{children}</ContentMain>
-          <Layout.Footer style={{ textAlign: 'center' }}>
-            Powered by <a href="http://moonrailgun.com">moonrailgun</a> with ❤
-          </Layout.Footer>
-        </Layout.Content>
+        {renderSidebar()}
+        {renderContent()}
       </Layout>
     </div>
   );
